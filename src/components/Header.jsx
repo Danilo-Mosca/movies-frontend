4a49505b4f893f1a3d7a4c961153dc42564402d1
Danilo-Mosca <danilo.mosca85@gmail.com>
2025-05-23 01:27:01 +0200

    added 3 header variants

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,144 @@
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
 function Header() {
     return (
-        <header>
-            <nav className="navbar bg-body-tertiary">
-                <div className="container-fluid">
-                    <a className="navbar-brand">Navbar</a>
-                    <form className="d-flex" role="search">
-                        <input
-                            className="form-control me-2"
-                            type="search"
-                            placeholder="Search"
-                            aria-label="Search"
-                        />
-                        <button className="btn btn-outline-success" type="submit">
-                            Search
-                        </button>
-                    </form>
-                </div>
-            </nav>
-        </header>
+        <>
+            <header>
+                <Navbar expand="md" className="bg-body-tertiary">
+                    <Container fluid>
+                        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="navbarScroll" />
+                        <Navbar.Collapse id="navbarScroll">
+                            <Nav
+                                className="me-auto my-2 my-md-0"
+                                style={{ maxHeight: '100px' }}
+                                navbarScroll
+                            >
+                                <Nav.Link href="#action1">Home</Nav.Link>
+                                <Nav.Link href="#action2">Link</Nav.Link>
+                                <NavDropdown title="Link" id="navbarScrollingDropdown">
+                                    <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action4">
+                                        Another action
+                                    </NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item href="#action5">
+                                        Something else here
+                                    </NavDropdown.Item>
+                                </NavDropdown>
+                                <Nav.Link href="#" disabled>
+                                    Link
+                                </Nav.Link>
+                            </Nav>
+                            <Form className="d-flex">
+                                <Form.Control
+                                    type="search"
+                                    placeholder="Search"
+                                    className="me-2"
+                                    aria-label="Search"
+                                />
+                                <Button variant="outline-success">Search</Button>
+                            </Form>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </header>
+
+            <header>
+                <Navbar collapseOnSelect expand="md" className="bg-body-tertiary">
+                    <Container>
+                        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link href="#features">Features</Nav.Link>
+                                <Nav.Link href="#pricing">Pricing</Nav.Link>
+                                <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
+                                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.2">
+                                        Another action
+                                    </NavDropdown.Item>
+                                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item href="#action/3.4">
+                                        Separated link
+                                    </NavDropdown.Item>
+                                </NavDropdown>
+                            </Nav>
+                            <Nav>
+                                <Nav.Link href="#deets">More deets</Nav.Link>
+                                <Nav.Link eventKey={2} href="#memes">
+                                    Dank memes
+                                </Nav.Link>
+                            </Nav>
+                            <Form className="d-flex">
+                                <Form.Control
+                                    type="search"
+                                    placeholder="Search"
+                                    className="me-2"
+                                    aria-label="Search"
+                                />
+                                <Button variant="outline-success">Search</Button>
+                            </Form>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </header>
+
+            <header>
+                <Navbar expand="md" className="bg-body-tertiary mb-3">
+                        <Container fluid>
+                            <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
+                            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`} />
+                            <Navbar.Offcanvas
+                                id={`offcanvasNavbar-expand-md`}
+                                aria-labelledby={`offcanvasNavbarLabel-expand-md`}
+                                placement="end"
+                            >
+                                <Offcanvas.Header closeButton>
+                                    <Offcanvas.Title id={`offcanvasNavbarLabel-expand-md`}>
+                                        Offcanvas
+                                    </Offcanvas.Title>
+                                </Offcanvas.Header>
+                                <Offcanvas.Body>
+                                    <Nav className="justify-content-end flex-grow-1 pe-3">
+                                        <Nav.Link href="#action1">Home</Nav.Link>
+                                        <Nav.Link href="#action2">Link</Nav.Link>
+                                        <NavDropdown
+                                            title="Dropdown"
+                                            id={`offcanvasNavbarDropdown-expand-md`}
+                                        >
+                                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                                            <NavDropdown.Item href="#action4">
+                                                Another action
+                                            </NavDropdown.Item>
+                                            <NavDropdown.Divider />
+                                            <NavDropdown.Item href="#action5">
+                                                Something else here
+                                            </NavDropdown.Item>
+                                        </NavDropdown>
+                                    </Nav>
+                                    <Form className="d-flex">
+                                        <Form.Control
+                                            type="search"
+                                            placeholder="Search"
+                                            className="me-2"
+                                            aria-label="Search"
+                                        />
+                                        <Button variant="outline-success">Search</Button>
+                                    </Form>
+                                </Offcanvas.Body>
+                            </Navbar.Offcanvas>
+                        </Container>
+                    </Navbar>
+            </header>
+        </>
     );
 };
 
