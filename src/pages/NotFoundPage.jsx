fd81e1b8b3da2bda0df0697c6ba53bf532af4a06
Danilo-Mosca <danilo.mosca85@gmail.com>
2025-06-04 19:56:12 +0200

    Added and customized not found page 404

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -2,10 +2,23 @@ import { Link } from "react-router-dom";
 
 function NotFoundPage() {
     return (
-        <div className=" d-flex justify-content-center align-items-center flex-column text-position-h1-p">
-            <h1 id="homepage-text">Errore 404</h1>
-            <p className="pt-sm-5" id="homepage-p">Pagina non trovata, torna alla <i><Link to="/">Homepage!</Link></i></p>
-        </div>
+        <section className="container p-5">
+            <div className="row pb-3 align-items-center">
+                <div className="col-12 col-md-6">
+                    <img
+                        src="/img/clapperboard-page-not-found.png"
+                        alt="Logo Movie App"
+                        className="p-5"
+                    />
+                </div>
+                <div className="col-12 col-md-6 text-center text-lg-start">
+                    <h1 id="homepage-text">OPS! Film o pagina non trovata!</h1>
+                    <p className="pt-3 pb-3">Non sono riuscito a trovare la pagina o il film che cercavi!</p>
+                    <Link to="/" className="btn-not-found">Torna alla Homepage!</Link>
+                    {/* style={{ width: "100%", height: "400px" }} */}
+                </div>
+            </div>
+        </section>
     );
 }
 
